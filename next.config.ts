434eaad9d8bdf8ba906f849bf49df7f7c57e924a
Mathieu Giguere <matduke@gmail.com>
2025-03-04 06:32:48 -0500

    sef

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  i18n: {
-    locales: ["en", "fr"], // Available languages
-    defaultLocale: "en", // Default language
-  },
+
 };
 
 export default nextConfig;
