ddce7f2c64fe1995dc3178c170015ea7605638df
lebred <matduke@gmail.com>
2025-03-03 20:05:04 -0500

    elfwf

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  i18n: {
+    locales: ['en', 'fr'], // Available languages
+    defaultLocale: 'en', // Default language
+  },
 };
 
 export default nextConfig;
+
