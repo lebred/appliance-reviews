5755463e955c89feb52054549f062be08c1957fe
lebred <matduke@gmail.com>
2025-03-03 20:08:19 -0500

    fwef

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   i18n: {
-    locales: ['en', 'fr'], // Available languages
-    defaultLocale: 'en', // Default language
+    locales: ["en", "fr"], // Available languages
+    defaultLocale: "en", // Default language
   },
 };
 
 export default nextConfig;
-
